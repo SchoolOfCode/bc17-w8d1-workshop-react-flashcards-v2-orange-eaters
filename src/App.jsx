67c5c21477a591902836d7630746fd634909be3f
kieranp-p6 <kiip96@icloud.com>
2024-08-19 13:22:18 +0100

    added visual x button

--- src/App.jsx
+++ src/App.jsx
@@ -11,31 +11,32 @@ function App() {
             <header>
                 <img src="logo_dark.svg" alt="React Logo"/>
                 <h1>React Flashcards</h1>
-        <p>🧠 Expand Your React Knowledge, One Flashcard at a Time! 🐌</p>
-      </header>
-      <main>
-        <form>
-          <label>Question:</label>
-          <input type="text" />
-          <label>Answer:</label>
-          <input type="text" />
-          <button type="submit">Add</button>
-        </form>
-        <section className="flashCards">
-            {questions.map((question) => {
-                return <div className="flashCard" key={question.question}>
-                    <p>🤔</p>
-                    <p>{question.question}</p>
-                </div>
-            })}
-        </section>
-      </main>
-      <footer>
-        <p>Build with React  <img src="logo_dark.svg" alt="React Logo" />
-        </p>
-      </footer>
-    </>
-  );
+                <p>🧠 Expand Your React Knowledge, One Flashcard at a Time! 🐌</p>
+            </header>
+            <main>
+                <form>
+                    <label>Question:</label>
+                    <input type="text"/>
+                    <label>Answer:</label>
+                    <input type="text"/>
+                    <button type="submit">Add</button>
+                </form>
+                <section className="flashCards">
+                    {questions.map((question) => {
+                        return <div className="flashCard" key={question.question}>
+                            <span className="deleteFlashCard">x</span>
+                            <p>🤔</p>
+                            <p>{question.question}</p>
+                        </div>
+                    })}
+                </section>
+            </main>
+            <footer>
+                <p>Build with React <img src="logo_dark.svg" alt="React Logo"/>
+                </p>
+            </footer>
+        </>
+    );
 }
 
 export default App;
